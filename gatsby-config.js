9d76c396bf9249b765479592a1c2acc0e6054a4e
mkhbragg <mk.bragg@pm.me>
2020-05-02 15:53:48 -0400

    Adding emotion and styled-system dependencies. Switching from npm to yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,24 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `The Fermenting Ape`,
+    description: `You have arrived at the front door for The Fermenting Ape! Good to see you.`,
+    author: `@mkhbragg`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: ["Sku"],
+        secretKey: process.env.STRIPE_SECRET_KEY,
+        downloadFiles: true,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
